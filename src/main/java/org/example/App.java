6c6584b6976ad0a83693c9fb0207761499204b87
mislavskii <jornjat@gmail.com>
2022-09-27 19:01:20 +0700

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+TODO:
+ Описать класс Birthday c двумя статическими методами:
+ int getAge(int year, int month, int date) — вернет возраст в днях
+ LocalDate nextBirthday(int year, int month, int date) — вернет дату следующего дня рождения в 1000 дней.
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
