fabd9e944d0a24792d0e4bfaf0d27ae86ba350a8
mislavskii <jornjat@gmail.com>
2022-10-10 11:57:35 +0700

    CRLF handling

--- src/main/java/org/example/StreamTransformer.java
+++ src/main/java/org/example/StreamTransformer.java
@@ -8,30 +8,35 @@ public class StreamTransformer implements Transformable{
     public void transform(String fileIn, String fileOut) {
 
         StringBuilder result = new StringBuilder();
-        FileInputStream stream;
+        FileInputStream inStream;
         FileOutputStream outStream;
         try {
-            stream = new FileInputStream(fileIn);
+            inStream = new FileInputStream(fileIn);
             outStream = new FileOutputStream(fileOut);
             int r;
             do {
-                r = stream.read();
-                if (r == '\n' || (r == -1)) {
-                    String[] arr = result.toString().split(";");
+                r = inStream.read();
+                if (r != -1) {
+                    result.append((char) r);
+                }
+                if (result.toString().endsWith("\n") || (r == -1)) {
+                    String line = result.toString();
+                    line = line.replace('\r', ' ').replace('\n', ' ').trim();
+                    String[] arr = line.split(";");
                     String angry = arr[2].equals("true") ? "Angry" : "Friendly";
-                    String outLine = String.format("%s cat %s, weight %s kg.", angry, arr[0], arr[1]) + "\n";
+                    String name = arr[0];
+                    String weight = arr[1];
+                    String outLine = String.format("%s cat %s, weight %s kg.", angry, name, weight) + "\n";
                     outStream.write(outLine.getBytes(StandardCharsets.UTF_8), 0, outLine.length());
                     result = new StringBuilder();
-                } else {
-                    result.append((char) r);
                 }
             } while (r != -1);
             outStream.flush();
             outStream.close();
+            inStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
